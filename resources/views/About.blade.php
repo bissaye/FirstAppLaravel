3bb696e5edb31fffbf3f64778ae0866b3cd885ec
bissaye <bissaye.com@gmail.com>
2022-09-24 10:41:40 +0100

    add about page and updat link

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -4,18 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>First app in laravel</title>
+        <title>About | First app in laravel</title>
     </head>
     <body>
-        <h1>
-            First web app in laravel
-        </h1>
-        <p>
-            It is curently {{ date('h:m A')}} .
-        </p>
+        <h3>
+            Build with &hearts; by me FRANCK BISSAYE <br>
+            An eternal code lover
+        </h3>
         <footer>
             <p>
-                &copy; CopyRight {{ date('Y') }} &middot; <a href="/about">about me</a>
+                <a href="/">return to home</a>
             </p>
         </footer>
     </body>
