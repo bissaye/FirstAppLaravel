03dab37f1942c2b10856877ece6956115e7c4d5f
bissaye <bissaye.com@gmail.com>
2022-09-24 10:30:36 +0100

    creation de la commande bssy pour le fun

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('bssy', function() {
+    $this->comment("bssy in jersey");
+})->purpose('display bssy in jersey');
