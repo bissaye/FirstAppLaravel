a7af8fabe98597d401b3aa550161ef5de86430d4
bissaye <bissaye.com@gmail.com>
2022-09-24 12:08:49 +0100

    attribution des noms au routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Routes;
 
 Route::get('/', function(){
     return View("pages/Home");
-});
+})->name('app_home');
 
 Route::get('about', function(){
     return View("pages/About");
-});
+})->name('app_about');;
