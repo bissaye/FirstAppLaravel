2d6d24d3ac5bcd2c665c2728e10068d76dc8cbf1
bissaye <bissaye.com@gmail.com>
2022-09-24 10:29:50 +0100

    creation de la vue home

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>First app in laravel</title>
+    </head>
+    <body>
+        <h1>
+            First web app in laravel
+        </h1>
+        <p>
+            It is curently {{ date('h:m A')}} .
+        </p>
+        <footer>
+            <p>
+                &copy; CopyRight {{ date('Y') }} &middot; <a href="http:#">about me</a>
+            </p>
+        </footer>
+    </body>
+</html>
